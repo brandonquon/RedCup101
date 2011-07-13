93cfffa6397d7a33b9e11380eae5708e5a3e04ad
bq <barbq@ubuntu.(none)>
2011-07-13 04:45:51 -0700

    first push

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Blog::Application.routes.draw do |map|
+ root :to => "articles#index"
+  resources :articles do
+    member do
+      post :notify_friend
+    end
+    resources :comments
+  end
+  resources :users
+  resource :session
+  resources :articles do
+    member do
+      post 'vote'
+    end
+  end
+  match '/login' => "sessions#new", :as => "login"
+  match '/logout' => "sessions#destroy", :as => "logout"  
+end
