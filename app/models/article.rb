4369a53e56fc83d5537a6145ae8fdfbd53ead879
bq <barbq@ubuntu.(none)>
2011-07-13 22:54:19 -0700

    reverse order

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,11 +2,11 @@ class Article < ActiveRecord::Base
   acts_as_voteable
   validates :title, :presence => true
   validates :body, :presence => true
- 
+  default_scope order('created_at DESC')
   belongs_to :user
   has_and_belongs_to_many :categories
   has_many :comments
- 
+  has_many :comments, :order => "created_at DESC" 
   scope :published, where("articles.published_at IS NOT NULL")
   scope :draft, where("articles.published_at IS NULL")
   scope :recent, lambda { published.where("articles.published_at > ?", 1.week.ago.to_date)}
